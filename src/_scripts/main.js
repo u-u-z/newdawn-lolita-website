8f917ec001dac66c7173516982576dbf99ab9d56
Remi_IO <i@linux.dog>
2019-10-09 00:04:18 +0800

    Ready for upload.

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -3,12 +3,12 @@
 
 'use strict';
 
-import $ from 'jquery';
+// import $ from 'jquery';
 import { Link } from '../_modules/link/link';
 
-$(() => {
+
   new Link(); // Activate Link modules logic
-  console.log('Welcome to Yeogurt!');
+  console.log('你来审计源代码啦！有没有兴趣来团队工作呢？email: i@linux.dog');
   
   let mbMenuStatus = false
   let menuButton = document.getElementById('mbmenubutton')
@@ -18,10 +18,10 @@ $(() => {
       menu.style.display = 'none'
     } else {
       menu.style.display = 'block'
+      menu.classList.add('animated', 'bounceInDown')
     }
     mbMenuStatus = !mbMenuStatus
   }
 
   let menu = document.getElementById('mbmenu')
   menu.style.display = 'none'
-});
