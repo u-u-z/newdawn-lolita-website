c26aa276bb77025bec21ea531d97375fafb35108
Remi_IO <i@linux.dog>
2019-10-08 19:59:24 +0800

    Add logic for button.

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -9,4 +9,19 @@ import { Link } from '../_modules/link/link';
 $(() => {
   new Link(); // Activate Link modules logic
   console.log('Welcome to Yeogurt!');
+  
+  let mbMenuStatus = false
+  let menuButton = document.getElementById('mbmenubutton')
+  menuButton.onclick = () => {
+    let menu = document.getElementById('mbmenu')
+    if (mbMenuStatus) {
+      menu.style.display = 'none'
+    } else {
+      menu.style.display = 'block'
+    }
+    mbMenuStatus = !mbMenuStatus
+  }
+
+  let menu = document.getElementById('mbmenu')
+  menu.style.display = 'none'
 });
